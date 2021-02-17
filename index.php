4ff5f4d443c36e95f7f40b57dfc9bfe883149b5e
Christian Zehetner <mail@christianzehetner.at>
2021-02-17 10:08:03 +0100

    snippet function loads twig files from standard snippet directory.

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 require_once __DIR__ . '/src/helpers.php';
 
 use Kirby\Cms\App;
+use Kirby\Toolkit\Tpl as Snippet;
 
 Kirby::plugin('amteich/twig', [
     'options' => [
@@ -12,6 +13,30 @@ Kirby::plugin('amteich/twig', [
     'components' => [
         'template' => function (App $kirby, string $name, string $contentType = 'html', string $defaultType = 'html') {
             return new amteich\Twig\Template($name, $contentType, $defaultType);
+        },
+        'snippet' => function (Kirby $kirby, string $name, array $data = []) {
+            $snippets = A::wrap($name);
+
+            foreach ($snippets as $name) {
+                $name = (string)$name;
+                $file = $kirby->root('snippets') . '/' . $name . '.php';
+
+                if (file_exists($file) === false) {
+                    $file = $kirby->root('snippets') . '/' . $name . '.twig';
+                    if (file_exists($file)) {
+                        return twig('@snippets/' . $name . '.twig');
+                    }
+                    else {
+                        $file = $kirby->extensions('snippets')[$name] ?? null;
+                    }
+                }
+
+                if ($file) {
+                    break;
+                }
+            }
+
+            return Snippet::load($file, $data);
         }
     ]
 ]);
